995409f7bde0b0fbd2e3e20ccea30979e25ede5e
Yaniv Illouz <yanivi@checkpoint.com>
2019-12-03 13:37:47 +0200

    initial

---
+++ server/keys.js
@@ -0,0 +1,10 @@
+module.export = {
+	redisHost: process.env.REDIS_HOST,
+	redisPort: process.env.REDIS_PORT,
+	pgUser: process.env.PGUSER,
+	pgHost: process.env.PGHOST,
+	pgDatabase: process.env.PGDATABASE,
+	pgPassword: process.env.PGPASSWORD,
+	pgPort: process.env.PGPORT
+	 
+};
